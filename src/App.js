33c10901a9ace966caba14d6f3c1a1daf6464224
collin <cbetori@gmail.com>
2019-07-18 10:18:17 -0700

    added libraries

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Funds from './components/funds'
+import { DatePicker } from 'antd'
 
 
 function App() {
   return (
     <div>
       <Funds />
+    <DatePicker />
     </div>
   );
 }
